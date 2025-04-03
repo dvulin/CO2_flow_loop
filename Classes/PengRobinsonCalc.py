18d8a64fa8f6057d8bae9c6fe0d2ce03aced05ab
ogmiosram <ogmiosram@yahoo.com>
2025-04-04 00:35:54 +0200

    PR overload Z

--- Classes/PengRobinsonCalc.py
+++ Classes/PengRobinsonCalc.py
@@ -12,6 +12,9 @@ class PengRobinsonCalc(EOSCalc):
         self.a_vals, self.b_vals = np.array(self.a_vals), np.array(self.b_vals)
         self.a_mix = np.sum(np.outer(self.z, self.z) * np.sqrt(np.outer(self.a_vals, self.a_vals)))
         self.b_mix = np.sum(self.z * self.b_vals)
+
+        self.A = (self.a_mix * self.P) / (self.R**2 * self.T**2)
+        self.B = (self.b_mix * self.P) / (self.R * self.T)
     
     def peng_robinson_params(self, T, comp):
         Tc, Pc, omega = comp["Tc"], comp["Pc"], comp["omega"]
@@ -24,11 +27,21 @@ class PengRobinsonCalc(EOSCalc):
 
     def peng_robinson_eos(self, V):
         return self.P - (self.R * self.T) / (V - self.b_mix) + (self.a_mix / (V**2 + 2 * self.b_mix * V - self.b_mix**2))
-    
-    def solve_eos(self):
-        V_guess = self.R * self.T / self.P
-        V_solution = fsolve(self.peng_robinson_eos, V_guess)
-        return V_solution[0]
+
+    def solve_eos(self, use_z_form=False):
+        if use_z_form:
+            # rješavanje kubne jednadžbe za Z
+            A, B = self.A, self.B
+            coeffs = [1, -(1 - B), A - 3*B**2 - 2*B, -(A*B - B**2 - B**3)]
+            roots = np.roots(coeffs)
+            real_roots = roots[np.isreal(roots)].real
+            Z = max(real_roots)  # pretpostavljamo plinsku fazu
+            V = Z * self.R * self.T / self.P
+            return V
+        else:
+            V_guess = self.R * self.T / self.P
+            V_solution = fsolve(self.peng_robinson_eos, V_guess)
+            return V_solution[0]
 
     def compute_k_factors(self, V):
         phi_L = np.exp((self.b_vals / self.b_mix) * (self.P * (V - self.b_mix) / (self.R * self.T) - 1))
